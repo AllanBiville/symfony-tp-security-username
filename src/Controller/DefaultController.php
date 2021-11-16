e19751a56afa342653d4114d117cc3c2bf928b0c
BIVILLE Allan <biviall@ndlp.fr>
2021-11-16 12:55:22 +0100

    Commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/home", name="homeg")
      */
     public function index(): Response
     {
